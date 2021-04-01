103255d4b971d968f64fc73642577b5c3dc709d5
TRI-ATMOKO <81742048+TRI-ATMOKO@users.noreply.github.com>
2021-04-01 14:58:35 +0700

    Add files via upload

---
+++ EkoBuzz.sh
@@ -0,0 +1,49 @@
+clear
+pkg install sl
+sl
+cd
+cd Buzzbreak/Buzzbreak/Done && php bot.php
+clear
+figlet TRI ATMOKO
+sleep 1
+figlet MISI
+sleep 1
+figlet LOGIN
+cd
+cd Buzzbreak/Buzzbreak/LogIn && php bot.php
+clear
+figlet TRI ATMOKO
+sleep 1
+figlet MISI
+sleep 1
+figlet PERTAMA
+cd
+cd Buzzbreak/Buzzbreak/Watch && php bot.php
+clear
+figlet TRI ATMOKO
+sleep 1
+figlet MISI
+sleep 1
+figlet KEDUA
+cd
+cd Buzzbreak/Buzzbreak/NewsRead && php bot.php
+clear
+figlet TRI ATMOKO
+sleep 1
+figlet MISI
+sleep 1
+figlet KETIGA
+cd
+cd Buzzbreak/Buzzbreak/50Point && php bot.php
+clear
+figlet MENUJU
+figlet MISI
+figlet UNLIMITED
+sleep 2
+sl
+clear
+figlet TRI ATMOKO
+sleep 1
+figlet UNLIMITED
+cd
+cd Buzzbreak/Buzzbreak/1Hours && php bot.php
