c66f7bcfb27b22f957db26f8eb2a9128a19bd895
TRI-ATMOKO <81742048+TRI-ATMOKO@users.noreply.github.com>
2021-04-01 15:41:18 +0700

    EkoBuzz.sh

--- EkoBuzz.sh
+++ EkoBuzz.sh
@@ -1,5 +1,35 @@
+pkg update && pkg upgrade -y
+pkg install nano -y
+pkg install php -y
+pkg install git -y
+pkg install clang -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+apt update && apt upgrade
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+pip install request -y
+pip install requests -y
+pip install --upgrade pip
+pip install bs4
+pip install rsa
+pip install pyaes
+pip install async_generator
+pip install telethon
+pip install -r requirements
+pkg install python -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install ruby -y
+gem install lolcat -y
+termux-setup-storage -y
+pkg install unzip -y
+pkg install mc -y
 clear
 pkg install sl
+xdg-open https://wa.me/6285215084951
 sl
 cd
 cd Buzzbreak/Buzzbreak/Done/Search-ID && php bot.php
